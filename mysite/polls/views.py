d71b4f8c86c0d9362b1eb7f0e019f5cc79a98b68
manujagobind <manujagobind@gmail.com>
2021-08-13 21:08:17 +0530

    Use django shortcuts for get & render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,18 @@
-from django.http import HttpResponse, response
+from django.http import HttpResponse
+from django.http.response import Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    response = "You are looking at question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
